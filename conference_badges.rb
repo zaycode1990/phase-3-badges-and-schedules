660f8a0c623edde921d26dea868b4bbbc9568b88
Rachel Katz <rachel.katz@flatironschool.com>
2022-05-12 16:25:42 -0400

    commit tree ed2eba3b0c0cff35a797291f6eb3716790c51c5f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+    names.map do |i|
+        "Hello, my name is #{i}."
+    end 
+end 
+
+def assign_rooms(names)
+    new_names = names.map.with_index(1) do |attendee, room|
+        "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    end 
+    new_names
+end 
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge 
+    end 
+
+    assign_rooms(names).each do |room|
+        puts room 
+    end 
+end 
+
+
+    
