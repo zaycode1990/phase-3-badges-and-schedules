d9a845455ca354fda0e9f01c9b36061f9ef14b13
Isaiah <ijandicos90@gmail.com>
2022-07-14 00:22:19 -0400

    Completed Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require "pry"
+guest = ["", ""]
+def badge_maker(name)
+    "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(*guest)
+["Hello, my name is #{guest[0]}", "Hello, my name is #{guest[1]}"]
+end
+
+def assign_rooms(*guest) 
+guest.map.with_index(1) do |name, index|
+"Hello, #{name}! You'll be assigned to room #{index}!"
+   end
+end
+
+def printer(printArr)
+batch_badge_creator(guest).each do |badge| 
+    puts badge
+    end
+assign_rooms(guest).each do |room| 
+    puts room
+    end
+end
+
+binding.pry
+
