e85f37c06ffe8cdc4791f242ebbbb7f577de2613
Isaiah <ijandicos90@gmail.com>
2022-07-14 21:29:33 -0400

    Completed Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,28 @@
 require "pry"
-guest = ["", ""]
+# guest = ["", ""]
 def badge_maker(name)
-    "Hello, my name is #{name}"
+    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(*guest)
-["Hello, my name is #{guest[0]}", "Hello, my name is #{guest[1]}"]
+def batch_badge_creator(guest)
+guest.map{|new_guest| "Hello, my name is #{new_guest}."}
 end
 
-def assign_rooms(*guest) 
+def assign_rooms(guest) 
 guest.map.with_index(1) do |name, index|
 "Hello, #{name}! You'll be assigned to room #{index}!"
    end
 end
 
-def printer(printArr)
+def printer(guest)
 batch_badge_creator(guest).each do |badge| 
     puts badge
     end
 assign_rooms(guest).each do |room| 
     puts room
     end
+    # add splat to for other functions in order to keep the array-like structure of the argument
 end
 
-binding.pry
+
 
